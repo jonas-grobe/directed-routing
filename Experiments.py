0af83380afd9a3b4bbca15dd71a0a437017f4ba4
jonas-grobe <143589622+jonas-grobe@users.noreply.github.com>
2023-12-02 10:37:09 +0100

    Update experiments

--- Experiments.py
+++ Experiments.py
@@ -15,7 +15,7 @@ metrics = [Evaluation.Packetloss, Evaluation.Stretch]
 # Add parameter filename, to write results to file
 # Add parameters draw/draw_route = True to draw graph
 
-# Fig. 6 + 10
+# Fig. 6 + 11
 Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.random_directed(
     n=25, d=6, p=0.1), fg.random_failure(), algos)
 Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.random_directed(
@@ -23,7 +23,7 @@ Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.random_directed(
 Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.random_directed(
     n=25, d=6, p=0.75), fg.random_failure(), algos)
 
-# Fig. 7 + 11
+# Fig. 7 + 12
 Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.erdos_renyi(
     n=25, p=0.35, directed=True), fg.random_failure(), algos)
 Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.erdos_renyi(
@@ -31,39 +31,48 @@ Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.erdos_renyi(
 Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.erdos_renyi(
     n=25, p=0.6, directed=True), fg.random_failure(), algos)
 
-# Fig. 8 + 9
+# Fig. 8 + 13
+Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.wireless(
+    n=25, area=50, min_range=10, max_range=20), fg.random_failure(), algos)
+Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.wireless(
+    n=50, area=80, min_range=15, max_range=25), fg.random_failure(), algos)
+Evaluation.eval_fr(500, "failure_rate", f_05, metrics, gg.wireless(
+    n=25, area=50, min_range=5, max_range=25), fg.random_failure(), algos)
+
+
+# Fig. 9 + 10
 Evaluation.eval_fr(500, "failure_rate", f_05, metrics,
                    gg.heathland(), fg.random_failure(), algos)
 
-# Fig. 12
+# Fig. 14
 Evaluation.eval(50, "n", [5, 10, 15, 20, 25, 30, 35, 40, 45, 50], [
                 Evaluation.Runtime], gg.erdos_renyi(n=5, p=0.35, directed=True), fg.no_failure(), algos)
 
-# Fig. 13
+# Fig. 15
 Evaluation.eval(100, "p", f_09 + [1], [Evaluation.ALinks, Evaluation.UsableALinks],
                 gg.erdos_renyi(n=50, p=0, directed=True), fg.no_failure(), [ra.KeepForwarding(version=1)])
 
-# Fig. 14
+# Fig. 16
 Evaluation.eval(100, "p", f_09 + [1], [Evaluation.DownLinks, Evaluation.ALinks, Evaluation.UpLinks],
                 gg.erdos_renyi(n=50, p=0, directed=True), fg.no_failure(), [ra.KeepForwarding()])
 Evaluation.eval(100, "failure_rate", f_08, [Evaluation.DownLinksRouted, Evaluation.ALinksRouted, Evaluation.UpLinksRouted],
                 gg.erdos_renyi(n=50, p=0.35, directed=True), fg.random_failure(), [ra.KeepForwarding()])
 
-# Fig. 15
+# Fig. 17
 Evaluation.eval(500, "p", [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1], [Evaluation.EdgesUsable, Evaluation.NumberOfArbs], gg.erdos_renyi(
     n=20, p=0, directed=True), fg.random_failure(failure_rate=0.5), [ra.Bonsai("greedy"), ra.Bonsai("round-robin"), ra.Grafting()])
 
-# Fig. 16
+# Fig. 18
 Evaluation.eval(500, "failure_rate", f_05, [Evaluation.Packetloss, Evaluation.Stretch], gg.erdos_renyi(
     n=50, p=0.35, directed=True), fg.random_failure(), [ra.KeepForwarding(), ra.KeepForwarding(version=1)])
 
-# Fig. 17
+# Fig. 19
 Evaluation.eval(500, "n", [10, 20, 30, 40, 50], [Evaluation.Packetloss, Evaluation.Stretch], gg.erdos_renyi(
     n=25, p=0.35, directed=True), fg.random_failure(failure_rate=0.5), algos)
 Evaluation.eval(500, "p", [0.1, 0.3, 0.5, 0.7, 0.9], [Evaluation.Packetloss, Evaluation.Stretch], gg.erdos_renyi(
     n=25, p=0.35, directed=True), fg.random_failure(failure_rate=0.5), algos)
 
-# Fig. 18
+# Fig. 20
 Evaluation.eval(500, "n", [10, 20, 30, 40, 50], [Evaluation.Packetloss, Evaluation.Stretch],
                 gg.random_directed(n=50, d=6, p=0.5), fg.random_failure(failure_rate=0.5), algos)
 Evaluation.eval(500, "d", [2, 4, 6, 8, 10, 12, 14, 16], [Evaluation.Packetloss, Evaluation.Stretch],
